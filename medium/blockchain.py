4893d0c3fce8e642b352a2a1c7f690858f3513b1
Jorge Yau <codenameyau@gmail.com>
2018-01-02 00:34:42 -0500

    Add skeleton and fixture block

--- medium/blockchain.py
+++ medium/blockchain.py
@@ -4,4 +4,21 @@ https://hackernoon.com/learn-blockchains-by-building-one-117428612f46
 '''
 
 class Blockchain():
-    pass
+
+    def __init__(self):
+        self.chain = []
+        self.transactions = []
+
+    def new_block(self):
+        pass
+
+    def new_transaction(self):
+        pass
+
+    @staticmethod
+    def hash(block):
+        pass
+
+    @property
+    def last_block(self):
+        pass
