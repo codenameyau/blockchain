25e862f4d8fc506319e18c28642658bfa1018aff
Jorge Yau <codenameyau@gmail.com>
2018-01-02 00:49:31 -0500

    Add new_transaction and new_block methods

--- medium/blockchain.py
+++ medium/blockchain.py
@@ -1,3 +1,7 @@
+import hashlib
+import json
+from time import time
+
 '''
 Step-by-step Guide
 https://hackernoon.com/learn-blockchains-by-building-one-117428612f46
@@ -9,11 +13,45 @@ class Blockchain():
         self.chain = []
         self.transactions = []
 
-    def new_block(self):
-        pass
+        # Create genesis block.
+        self.new_block(proof=100, previous_hash=1)
 
-    def new_transaction(self):
-        pass
+    def new_block(self, proof, previous_hash=None):
+        '''
+        Creates a new block in the blockchain.
+
+        :param proof: <int> Proof given by our proof of work algorithm.
+        :param previous_hash: <str> Hash of previous block.
+        :return: <dict> New block
+        '''
+        block = {
+            'index': len(self.chain) + 1,
+            'timestamp': time(),
+            'transactions': self.transactions,
+            'proof': proof,
+            'previous_hash': previous_hash or self.hash(self.chain[-1])
+        }
+
+        self.transactions = []
+        self.chain.append(block)
+        return block
+
+    def new_transaction(self, sender, recipient, amount):
+        '''
+        Creates a new transaction for the next mined block.
+
+        :param sender: <str> Address of the sender.
+        :param recipient: <str> Address of the recipient.
+        :param amount: <int> Amount.
+        :return: <int> The index of the block for this transaction.
+        '''
+        transaction = {
+            'sender': sender,
+            'recipient': recipient,
+            'amount': amount
+        }
+        self.transactions.append(transaction)
+        return self.last_block.get('index') + 1
 
     @staticmethod
     def hash(block):
@@ -21,4 +59,4 @@ class Blockchain():
 
     @property
     def last_block(self):
-        pass
+        return self.transactions[-1]
